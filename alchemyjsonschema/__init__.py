cee81f707b3ebb64e72fd555b9118c76cf127caf
Matthew Richardson <M.Richardson@ed.ac.uk>
2021-03-29 17:06:07 +0100

    Drop deprecated sqlalchemy Binary types.

--- alchemyjsonschema/__init__.py
+++ alchemyjsonschema/__init__.py
@@ -52,8 +52,6 @@ default_column_to_schema = {
     t.DateTime: "string",
     t.Date: "string",
     t.Time: "string",  # xxx
-    t.LargeBinary: "xxx",
-    t.Binary: "xxx",
     t.Boolean: "boolean",
     t.Unicode: "string",
     t.Concatenable: "xxx",
